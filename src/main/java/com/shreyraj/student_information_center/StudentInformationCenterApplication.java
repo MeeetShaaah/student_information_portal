8fcf805dc95b2fb0862966110b79374a79a551c0
MeeetShaaah <meetshah7199@gmail.co>
2024-01-25 16:46:14 -0500

    Start With the Project

---
+++ src/main/java/com/shreyraj/student_information_center/StudentInformationCenterApplication.java
@@ -0,0 +1,13 @@
+package com.shreyraj.student_information_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInformationCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentInformationCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreyraj/student_information_center/StudentInformationCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyraj.student_information_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentInformationCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
