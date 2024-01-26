6ecba18582f8b5b4e674d1a95110dc987908412d
MeeetShaaah <meetshah7199@gmail.co>
2024-01-26 14:58:12 -0500

    Update App.js

--- sic_client/src/App.js
+++ sic_client/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Home from "./Home";
+import NavBar from "./component/common/NavigationBar";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from "react-router-dom";
+
+import AddStudent from "./component/student/AddStudent";
+import EditStudent from "./component/student/EditStudent";
+import StudentPofile from "./component/student/StudentProfile";
+import StudentsView from "./component/student/StudentsView";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main className="container mt-5">
+			<Router>
+				<NavBar />
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={<Home />}></Route>
+					<Route
+						exact
+						path="/view-students"
+						element={<StudentsView />}></Route>
+					<Route
+						exact
+						path="/add-students"
+						element={<AddStudent />}></Route>
+					<Route
+						exact
+						path="/edit-student/:id"
+						element={<EditStudent />}></Route>
+					<Route
+						exact
+						path="/student-profile/:id"
+						element={<StudentPofile />}></Route>
+				</Routes>
+			</Router>
+		</main>
+	);
 }
 
 export default App;
